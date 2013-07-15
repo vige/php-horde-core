353504bbd26ea548c85c424c626a7e4acd53da83
Mathieu Parent <math.parent@gmail.com>
2013-07-15 17:10:29 +0200

    Imported Upstream version 2.6.1

---
+++ Horde_Core-2.6.1/lib/Horde/Core/Factory/History.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @category Horde
+ * @package  Core
+ */
+class Horde_Core_Factory_History extends Horde_Core_Factory_Injector
+{
+    public function create(Horde_Injector $injector)
+    {
+        if (empty($GLOBALS['conf']['sql']['phptype']) ||
+            ($GLOBALS['conf']['sql']['phptype'] == 'none')) {
+            throw new Horde_Exception(Horde_Core_Translation::t("The History system is disabled."));
+        }
+
+        $history = new Horde_History_Sql(
+            $injector->getInstance('Horde_Registry')->getAuth(),
+            $injector->getInstance('Horde_Core_Factory_Db')->create('horde', 'history')
+        );
+
+        if (is_callable(array($history, 'setCache')) &&
+            ($cache = $injector->getInstance('Horde_Cache'))) {
+            $history->setCache($cache);
+        }
+
+        return $history;
+    }
+}
